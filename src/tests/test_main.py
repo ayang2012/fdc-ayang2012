7a7ccd3336d288e0e6ceb57322268a3896fadead
Aaron Yang <ayang2012@gmail.com>
2024-05-05 21:18:01 -0500

    feat: include test for baseline for transform function

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,6 +1,11 @@
-from fig_data_challenge.main import return_42
+from fig_data_challenge.main import transform
+import pandas as pd
+from pandas.testing import assert_frame_equal
 
 
-def test_main():
-    value = return_42()
-    assert value == 42
+def test_transform():
+    test_df = pd.read_csv('src/tests/test_data/df_raw_sample_10_42.csv')
+    df = transform(test_df).reset_index(drop=True)
+    result_df = pd.read_csv('src/tests/test_data/df_raw_sample_10_42_result.csv')
+    assert_frame_equal(df, result_df)
+
