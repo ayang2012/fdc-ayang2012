80cef03c9dc3700542c21465d7398056bd272ce5
Aaron Yang <ayang2012@gmail.com>
2024-05-06 14:41:41 -0500

    fix: changed tests to reflect new category matching

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -5,6 +5,7 @@ from pandas.testing import assert_frame_equal
 
 def test_transform():
     test_df = pd.read_csv("src/tests/test_data/df_raw_sample_10_42.csv")
-    df = transform(test_df).reset_index(drop=True)
+    ref_df =  pd.read_excel('data/restaurant_data.xlsx', sheet_name="Reference categories")
+    df = transform(test_df, ref_df).reset_index(drop=True)
     result_df = pd.read_csv("src/tests/test_data/df_raw_sample_10_42_result.csv")
     assert_frame_equal(df, result_df)
